d4f59ef26a1b9d5d507688d71399734c26365c75
FreeTime010 <time0009000@gmail.com>
2022-04-05 14:37:28 +0700

    change

--- fighting_game/index.js
+++ fighting_game/index.js
@@ -1,59 +1,32 @@
-const canvans = document.querySelector('canvas');
-const c = canvans.getContext('2d');
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
-canvans.width = 1024
-canvans.height = 576
+canvas.width = 1024
+canvas.height = 576
 
-c.fillRect(0, 0, canvans.width, canvans.height);
+c.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.2
-
-// create class
 class Sprite {
-    constructor({ position, velocity }) {
-        this.position = position;
-        this.velocity = velocity
-        this.height = 150
+    constructor(position) {
+        this.position = position
     }
 
     draw() {
         c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
-    }
-
-    update() {
-        this.draw()
-        
-        this.position.y += this.velocity.y
-
-        if (this.position.y +this.height + this.velocity.y >= canvans.height) {
-            this.velocity.y = 0
-        } else this.velocity.y += gravity
+        c.fillRect(this.position.x, this.position.y, 50, 150)
     }
 }
 
 const player = new Sprite({
-    position: {
-        x: 0,
-        y: 0
-    },
-    velocity: {
-        x: 0,
-        y: 0
-    }
+    x: 0,
+    y: 0
 })
 
 player.draw()
 
 const enemy = new Sprite({
-    position: {
-        x: 400,
-        y: 100
-    },
-    velocity: {
-        x: 0,
-        y: 0
-    }
+    x: 400,
+    y: 100
 })
 
 enemy.draw()
@@ -61,11 +34,6 @@ enemy.draw()
 console.log(player)
 
 function animate() {
-    window.requestAnimationFrame(animate)
-    c.fillStyle = 'black'
-    c.fillRect(0, 0, canvans.width, canvans.height)
-    player.update()
-    enemy.update()
-}
+    
 
-animate()
+}
