c7102164491294068e092d545441a0088a2782a0
Kizuna <time0009000@gmail.com>
2022-06-03 08:38:55 +0700

    06/03/2022

--- MyWeb/public_html/index.js
+++ MyWeb/public_html/index.js
@@ -12,10 +12,19 @@ canvas.height = window.innerHeight;
 // c ~ content
 var c = canvas.getContext('2d');
 
+c.fillStyle = 'rgba(255, 0, 0, 0.1)';
 c.fillRect(100, 100, 100, 100);
-c.fillRect(100, 300, 100, 100);
+c.fillStyle = 'rgba(0, 255, 0, 0.1)';
+c.fillRect(400, 100, 100, 100);
+c.fillStyle = 'rgba(0, 0, 255, 0.1)';
+c.fillRect(300, 300, 100, 100);
 console.log(canvas);
 
 
 // line
 c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "#fa34a3";
+c.stroke();
